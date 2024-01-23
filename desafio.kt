374046a3b4f1147e789596d4852a874b040f695e
Felipe Sadrak dos Santos <141746637+Felip3Sadrak@users.noreply.github.com>
2024-01-23 15:50:36 -0300

    :sparkles:CONCLUSÃO DO DESAFIO DIO
    conclusão do desafio do bootcamp de desenvolvimento Backend em Kotlin.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,66 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(
+    val nome: String,
+    val duracao: Int = 60,
+    val descricao: String,
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+class Formacao(
+    val nome: String,
+    val nivel: Nivel,
+    var conteudos: MutableList<ConteudoEducacional> = mutableListOf()
+) {
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome.")
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        println("Conteúdo '${conteudo.nome}' adicionado à formação $nome.")
+    }
+
+    fun removerConteudo(conteudo: ConteudoEducacional) {
+        conteudos.remove(conteudo)
+        println("Conteúdo '${conteudo.nome}' removido da formação $nome.")
+    }
+
+    fun exibirDetalhes() {
+        println("Formação: $nome")
+        println("Nível: $nivel")
+        println("Conteúdos:")
+        for (conteudo in conteudos) {
+            println(" - ${conteudo.nome} (${conteudo.duracao} minutos) - ${conteudo.descricao} ")
+        }
+        println("Inscritos:")
+        for (inscrito in inscritos) {
+            println(" - ${inscrito.nome} ")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando  usuários e conteúdos educacionais como base
+    val usuario1 = Usuario("Felipe")
+    val usuario2 = Usuario("Sadrak")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Python", 90, "Conceitos básicos de Python")
+    val conteudo2 = ConteudoEducacional("Python Avançado", 200, "Tópicos avançados de Python")
+
+    // Criando uma formação e adicionando conteúdos
+    val formacaoKotlin = Formacao("Formação Python", Nivel.INTERMEDIARIO)
+    formacaoKotlin.adicionarConteudo(conteudo1)
+    formacaoKotlin.adicionarConteudo(conteudo2)
+
+    // Matriculando usuários na formação
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    // Exibindo detalhes da formação
+    formacaoKotlin.exibirDetalhes()
 }
